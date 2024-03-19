4b577349660cfa0045d0371eccd0f9c11bb3004a
WoutVanAelPXL <73279096+WoutVanAelPXL@users.noreply.github.com>
2024-03-19 15:45:29 +0100

    opening pdf works on Android

--- ExpoTestApp/App.js
+++ ExpoTestApp/App.js
@@ -3,6 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
+import * as IntentLauncher from 'expo-intent-launcher';
 
 export default function App() {
   const [image, setImage] = useState(null);
@@ -33,8 +35,19 @@ export default function App() {
       if (result.assets[0].mimeType.startsWith("image/")) {
         setImage(result.assets[0].uri);
       } else if (result.assets[0].mimeType == 'application/pdf') {
-        // open pdf here
-        // Didn't find a solution for Expo
+        try {
+          // Get content URI using FileProvider, WITHOUT THIS OTHER APPS CANNOT READ THE DATA
+          const contentUri = await FileSystem.getContentUriAsync(result.assets[0].uri);
+
+          // Open the saved PDF file --- IntentLauncher is Android only
+          await IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
+            data: contentUri,
+            flags: 1,
+            type: result.assets[0].mimeType,
+          });
+        } catch (error) {
+          console.error('Error saving or opening PDF: ', error);
+        }
       }
     }
   }
